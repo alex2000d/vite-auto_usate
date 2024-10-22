27c9e3fc02dbec5b81262251a7c5e1053e03491c
andrearomeo04 <pisco004@gmail.com>
2024-10-22 21:03:02 +0200

    [UPD] fixed card error

--- src/components/AutoCard.vue
+++ src/components/AutoCard.vue
@@ -7,14 +7,21 @@ export default {
 }
 </script>
 <template>
+    <div class="col-4 mb-3">
         <div class="card">
-             <img class="img-card-top" :src="car.image != null ? `http://127.0.0.1:8000/storage/${car.image}` : 'https://placehold.co/600x400?text=Immagine+copertina'">
-                 <div class="card-body">
-                     <h4 class="card-title">{{ car.brand }}</h4>
-                     <p class="card-text">{{ car.model }}</p>
-                </div>
-         </div>
+            <img class="img-card-top"
+                :src="car.image && car.image.startsWith('http') ? car.image : `http://127.0.0.1:8000/storage/${car.image}`">
+            <div class=" card-body">
+                <h4 class="card-title">{{ car.brand }}</h4>
+                <p class="card-text">{{ car.model }}</p>
+            </div>
+        </div>
+    </div>
 </template>
-<style lang="">
-    
+<style lang="scss" scoped>
+.img-card-top {
+    width: 100%;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+}
 </style>
--- src/pages/PageAuto.vue
+++ src/pages/PageAuto.vue
@@ -3,21 +3,21 @@ import axios from 'axios';
 import AutoCard from '../components/AutoCard.vue';
 
 export default {
-    components:{
+    components: {
         AutoCard
     },
     data() {
         return {
-        autos: [],  
+            autos: [],
         }
     },
-   created() {
-      this.getAutos();
-   },
+    created() {
+        this.getAutos();
+    },
     methods: {
-        getAutos(){
-            axios.get('http://127.0.0.1:8000/api/autos').then((results) =>{
-              this.autos = results.data.results;
+        getAutos() {
+            axios.get('http://127.0.0.1:8000/api/autos').then((results) => {
+                this.autos = results.data.results;
             });
         }
     },
@@ -31,12 +31,10 @@ export default {
             </div>
         </div>
         <div class="row">
-            <div class="col-4">
-                <AutoCard  v-for="auto in autos" :key="auto.id" :car="auto" />  
-            </div>
+            <AutoCard v-for="auto in autos" :key="auto.id" :car="auto" />
         </div>
     </div>
 </template>
 <style lang="">
-    
+
 </style>
